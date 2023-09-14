3bf7df038c1d6ff539b34460b84a5271719f7573
Maximiliano Levi <maxilevi77@gmail.com>
2023-09-14 16:11:58 +0200

    feat: script to verify layers

--- scripts/verify_layers.py
+++ scripts/verify_layers.py
@@ -2,11 +2,27 @@ from transformers import VitsModel, AutoTokenizer
 import scipy
 import torch
 
+def load_tensor_from_file(filename):
+    with open(filename, 'r') as f:
+        lines = f.readlines()
+        n_dims = int(lines[0].split()[0])
+        shape = tuple(map(int, lines[0].split()[1:]))
+        flat_data = []
+        for line in lines[1:]:
+            for value in line.split():
+                flat_data.append(float(value))
+        return torch.tensor(flat_data).reshape(shape)
+
 def verify_text_encoder(model, input_ids):
+    input_padding_mask = torch.ones_like(input_ids).unsqueeze(-1).float()
     text_encoder_output = model.text_encoder(
-        input_ids=input_ids
+        input_ids=input_ids,
+        padding_mask=input_padding_mask,
     )
     print(text_encoder_output)
+    cpp_output = load_tensor_from_file('./debug/text_encoder_output.txt')
+
+    assert torch.allclose(text_encoder_output, cpp_output, atol=1e-3)
 
 
 def verify_all(text):
@@ -14,6 +30,7 @@ def verify_all(text):
 
     tokenizer = AutoTokenizer.from_pretrained("facebook/mms-tts-spa")
     input_ids = tokenizer(text, return_tensors="pt").input_ids
+    print(f"Input ids: {input_ids} for '{text}'")
     verify_text_encoder(model, input_ids)
 
 
--- src/debug.h
+++ src/debug.h
@@ -5,4 +5,31 @@
 #ifndef VITS_CPP_DEBUG_H
 #define VITS_CPP_DEBUG_H
 
+#include <iostream>
+#include <fstream>
+
+#define SHAPE(tensor) printf("Shape '%s': %d x %d x %d x %d\n", ##tensor, tensor->shape[0], tensor->shape[1], tensor->shape[2], tensor->shape[3]);
+
+#define SAVE_LAYER(tensor, name) \
+    do { \
+        std::ofstream outfile("./debug/" + name + ".txt"); \
+        if(!outfile) { \
+            std::cerr << "Error opening file " << name << std::endl; \
+            break; \
+        } \
+        float* data = ggml_data_f32(tensor); \
+        outfile << tensor->n_dims; \
+        for(int i = 0; i < tensor->n_dims; i++) { \
+            outfile << " " << tensor->ne[i]; \
+        } \
+        outfile << std::endl; \
+        for(int i = 0; i < tensor->n_dims; i++) { \
+            for(int j = 0; j < tensor->ne[i]; j++) { \
+                outfile << data[i * tensor->ne[i] + j] << " "; \
+            } \
+            outfile << std::endl; \
+        } \
+        outfile.close(); \
+    } while(0)
+
 #endif //VITS_CPP_DEBUG_H
